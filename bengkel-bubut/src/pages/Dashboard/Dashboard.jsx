6a4428d0808068c7bd28b4097d2183cad28878af
William Limanto <limantowilliam05@gmail.com>
2022-12-06 19:15:43 +0700

    progress

--- bengkel-bubut/src/pages/Dashboard/Dashboard.jsx
+++ bengkel-bubut/src/pages/Dashboard/Dashboard.jsx
@@ -1,14 +1,23 @@
 import { Row } from "reactstrap";
 import Breadcrumbs from "../../components/BreadCrumbs.jsx";
 import icon from "../../Images/notSelected/dashboard.png";
-import {useLocation} from 'react-router-dom';
+import { useLocation } from "react-router-dom";
+import styles from "../../styles/Dashboard.module.css";
 
 const Dashboard = (props) => {
-    
   return (
-    <Row>
-      <Breadcrumbs icon={icon} activeName="Dashboard" />
-    </Row>
+    <>
+      <Row>
+        <Breadcrumbs icon={icon} activeName="Dashboard" />
+      </Row>
+      <div className={styles.leftTop}>
+        <div>
+          PENJUALAN HARI INI
+        </div>
+        <div>TOTAL TRANSAKSI</div>
+        <div>TOTAL PELANGGAN</div>
+      </div>
+    </>
   );
 };
 
