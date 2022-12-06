aba2e73f91a286f120cfd6b43e298fb9c096ad06
William Limanto <limantowilliam05@gmail.com>
2022-12-06 19:16:31 +0700

    add backend

---
+++ backend/workshopInformationSystem/src/main/java/com/example/workshopInformationSystem/WorkshopInformationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.workshopInformationSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopInformationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopInformationSystemApplication.class, args);
+	}
+
+}
---
+++ backend/workshopInformationSystem/src/test/java/com/example/workshopInformationSystem/WorkshopInformationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workshopInformationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopInformationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
