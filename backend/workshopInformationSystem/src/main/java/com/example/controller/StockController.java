92f105b713ba9d8f7df7646e46849f0ccc75bf11
William Limanto <limantowilliam05@gmail.com>
2022-12-06 22:29:53 +0700

    backend

---
+++ backend/workshopInformationSystem/src/main/java/com/example/controller/StockController.java
@@ -0,0 +1,26 @@
+package com.example.controller;
+
+import com.example.model.Stock;
+import com.example.service.StockService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/stock")
+@CrossOrigin
+public class StockController {
+    @Autowired
+    private StockService stockService;
+
+    @PostMapping("/add")
+    public String add(@RequestBody Stock stock){
+        stockService.saveStock(stock);
+        return "New Stock is added";
+    }
+  
+}
---
+++ backend/workshopInformationSystem/src/main/java/com/example/service/StockService.java
@@ -0,0 +1,7 @@
+package com.example.service;
+
+import com.example.model.Stock;
+
+public interface StockService {
+    public Stock saveStock(Stock stock);
+}
