fe18104b62a14fbbca52120f9524388118b0db2a
Danis <daniswaragozaly@gmail.com>
2022-12-09 20:30:20 +0700

    service add stock

--- backend/workshopInformationSystem/src/main/java/com/example/workshopInformationSystem/service/StockServiceImpl.java
+++ backend/workshopInformationSystem/src/main/java/com/example/workshopInformationSystem/service/StockServiceImpl.java
@@ -1,6 +1,8 @@
 package com.example.workshopInformationSystem.service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.example.workshopInformationSystem.model.Stock;
 import com.example.workshopInformationSystem.repository.StockRepository;
@@ -15,8 +17,24 @@ public class StockServiceImpl implements StockService {
 
     @Override
     public Stock saveStock(Stock stock){
-        return stockRepository.save(stock);
+        try {            
+
+            Stock stocks = new Stock();
+            stocks.setName(stock.getName());
+            stocks.setPrice(stock.getPrice()); 
+            stocks.setQuantity(stock.getQuantity());
+            
+            stockRepository.save(stocks);
+
+            Map<String, Object> userData = new HashMap<>();
+            userData.put("user", stock);
+            return stocks;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
+    
 
     @Override
     public List<Stock> getAllStocks() {
