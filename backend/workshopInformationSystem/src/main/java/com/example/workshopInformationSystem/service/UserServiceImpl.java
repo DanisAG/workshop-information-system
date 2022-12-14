fe18a7919a49357d1aa364d2ad0a401f405d70b5
Danis <daniswaragozaly@gmail.com>
2022-12-14 20:50:13 +0700

    update token

--- backend/workshopInformationSystem/src/main/java/com/example/workshopInformationSystem/service/UserServiceImpl.java
+++ backend/workshopInformationSystem/src/main/java/com/example/workshopInformationSystem/service/UserServiceImpl.java
@@ -69,6 +69,17 @@ public class UserServiceImpl implements UserService {
             queryResult.setParameter("username", user.getUsername());
             queryResult.setParameter("password", user.getPassword());
             User resultList = (User) queryResult.getSingleResult();
+
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(new Date());
+            calendar.add(Calendar.HOUR_OF_DAY, 12);
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            sdf.setTimeZone(TimeZone.getTimeZone("Asia/Jakarta"));
+            Date dtNow = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(sdf.format(calendar.getTime()));
+            resultList.setExpiredDate(dtNow);
+            resultList.setToken(new CommonMethod().generateToken());
+            userRepository.save(resultList);
+
             return resultList;
         } catch (Exception e) {
             // e.printStackTrace();
